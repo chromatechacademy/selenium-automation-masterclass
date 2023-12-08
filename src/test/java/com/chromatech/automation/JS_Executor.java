509515c16aab416ed66c452ede6c714ae21e79c8
chromatechacademy <team@chromatechacademy.com>
2023-12-08 15:22:30 -0500

    JavascriptExecutor interface

---
+++ src/test/java/com/chromatech/automation/JS_Executor.java
@@ -0,0 +1,41 @@
+package com.chromatech.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class JS_Executor {
+
+    public static void main(String[] args) {
+        /*
+         * TOPIC: JavascriptExecutor
+         */
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        String url = "https://chroma-tech-academy.mexil.it/static_page/";
+        driver.get(url);
+
+        // SCROLLING DOWN USING ACTIONS CLASS
+        // Actions actions = new Actions(driver);
+
+        WebElement hoverButton = driver.findElement(By.xpath("//button[normalize-space()='Mouse Hover']"));
+        // actions.moveToElement(hoverButton).perform();
+
+        // SCROLLING DOWN USING JAVASCRIPTEXECUTOR
+        // CAST THE WEBDRIVER INSTANCE TO JavascriptExecutor:
+        // JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        // jsExecutor.executeScript("arguments[0].scrollIntoView(true);", hoverButton);
+
+        // CLICKING AN ELEMENT USING JavascriptExecutor
+        // WebElement contactUsLink = driver.findElement(By.xpath("//a[normalize-space()='Contact info']"));
+        // jsExecutor.executeScript("arguments[0].click();", contactUsLink);
+
+    }
+
+}
